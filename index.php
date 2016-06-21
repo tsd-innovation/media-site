8713864a82270a8df92b8d1d675301e1c7de732b
andrewmather2 <andrewmather2@yahoo.com>
2016-06-21 07:55:46 -0700

    Better advertising layout

--- index.php
+++ index.php
@@ -199,7 +199,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-9">
+                <div class="col-md-12">
                 	<div class="panel panel-primary">
                     	<div class="panel-heading"><strong>Newspaper print rates</strong></div>
                        	<table class="table table-hover">
@@ -278,13 +278,20 @@
                         </table>
                     </div>
                 </div>
-                <div class = "col-md-3">
-                </div>
             </div>
             <div class = "row">
             	<div class = "col-md-4">
                 	<h3><strong>Premium placement</strong></h3>
                     <h4>Pay a <strong>5%</strong> premium to place an ad on Page 2, 3 or Back Cover (color required on Page 3 or Back Cover)</h4>
+                    <h3><strong>Front page stickers</strong></h3>
+                    <h4>
+                    	<p><strong>$600</strong></p>
+                        <p>Ship inserts and stickers to:</p>
+                        <p>Fricke Parks Press<br />33250 Transit Avenue, Union City, California, 94587</p>
+                    </h4>
+                    <p><em>Full press run of 8,000 copies required. Materials must be received one week prior to publication.</em></p>
+                </div>
+            	<div class = "col-md-4">
                 	<h3><strong>Discounts</strong></h3>
                     <ul class = "list-group">
                         <li class = "list-group-item">Buy 5 ads, take 5% off column-inch rate</li>
@@ -292,21 +299,11 @@
                         <li class = "list-group-item">Buy 15 ads, take 15% off column-inch rate</li>
                         <li class = "list-group-item">Buy 25 ads, take 25% off column inch rate</li>
                     </ul>
-                    <em>25% non-profit discount (only non-Stanford accounts)</em>          
-                </div>
-            	<div class = "col-md-4">
-                	<h3><strong>Front page stickers</strong></h3>
-                    <h4>
-                    	<p><strong>$600</strong></p>
-                        <p>Ship inserts and stickers to:</p>
-                        <p>Fricke Parks Press<br />33250 Transit Avenue, Union City, California, 94587</p>
-                        <p><em>Full press run of 8,000 copies required. Materials must be received one week prior to publication.</em></p>
-                    </h4>
-                	<h3><strong>Custom sizing</strong></h3>
+                   	<h3><strong>Custom sizing</strong></h3>
                     <h4>
                     	<p>Other sizes available! Just chose your height and number of column widths.</p>
-                        <p><a href = "#">See size chart</a></p>
                     </h4>
+                    <p><em><a href = "#">See size chart</a></em></p>
                 </div>
                 <div class = "col-md-4">
                 	<h3><strong>Special issue sponsorship</strong></h3>
