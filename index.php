2d8d6aa97558d253fb079eac0ca3de8ac766d6ac
andrewmather2 <andrewmather2@yahoo.com>
2016-07-13 14:22:30 -0700

    Converted image to jpeg

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
         	<div class="row">
                 <div class = "col-md-7">
                     <div id = "newspaper-image">
-                        <img class = "newspaper-main" src = "images/stanford-daily-boehner.png" title = "The Stanford Daily" />
+                        <img class = "newspaper-main" src = "images/stanford-daily-boehner.jpg" title = "The Stanford Daily" />
                     </div>
                 </div>
                 <div class = "col-md-5">
