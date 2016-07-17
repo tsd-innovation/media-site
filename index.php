32ffe6696c9ed43fc87e975afe0c48ccd4f39ed3
andrewmather2 <andrewmather2@yahoo.com>
2016-07-16 17:56:01 -0700

    Attempted change in section style

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
     <style>
 		h1{font-size: 30pt !important;}
 		h4 strong{color: #AC0000; font-size: 18pt; font-weight: bold;}
-        .dark-background h4 strong {color: #FFFFFF;}
-        .dark-background h1 {color: #FFFFFF;}
+        .dark-background h4 strong {color: #FFFFFF !important;}
+        .dark-background h1 {color: #FFFFFF !important;}
 		
 		button{margin: 0px 3px !important;}
 
@@ -27,7 +27,7 @@
 		.norm-bold{font-weight: bold;}
         .dark-background{background: #AC0000; z-index: 998;}
 
-		#newspaper-image{width: 80%; margin-left: 10%; margin-right: 10%; box-shadow: 4px 0px 4px -2px #888888; max-height: 100%; z-index: 100;}
+		#newspaper-image{width: 80%; margin-left: 10%; margin-right: 10%; box-shadow: 4px 0px 4px -2px #888888; max-height: 95%; z-index: 100;}
 		#sales-link{color: #F00; !important;}
 
 	</style>
