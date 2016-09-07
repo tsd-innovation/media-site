9e254c330c050e45abe8dc7be431b0dae32a0adf
andrewmather2 <andrewmather2@yahoo.com>
2016-09-07 10:13:01 -0700

    Updated text

--- index.php
+++ index.php
@@ -283,7 +283,7 @@
                 </div>
                 <div class="col-md-5">
                 	<h4>
-                    	<p>The Stanford Daily's <a href="stanforddaily.com">website</a> is the portal for more than 200,000 visitors each month to view our comprehensive coverage of the Farm. All articles from the print edition of the Daily appear on stanforddaily.com, along with a number of digital exclusives like premium video content and in-depth  </p>
+                    	<p>The Stanford Daily's <a href="stanforddaily.com">website</a> is the portal for more than 200,000 visitors each month to view our comprehensive coverage of the Farm. All articles from the print edition of the Daily appear on stanforddaily.com, along with a number of digital exclusives like premium video content and web-only blog posts.</p>
                         <p>Web ads complement print ads by appealing to different demographics and reaching additional members of the Stanford community who aren&nbsp;t within the reach of the Palo Alto campus. Students frequently use our webiste as well, and hundreds of  have taken an action based on one of our web ads.</p>
                         <p>Our website features one leaderboard ad and one sidebar ad available for purchase. Rates for these ads start at <strong class="num">$15</strong> CPM.</p>
                     </h4>
