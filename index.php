b292512bbabf1ed083f242a5e7c94da3a5591678
andrewmather2 <andrewmather2@yahoo.com>
2016-09-26 16:48:53 -0700

    Attempting cascade fix

--- index.php
+++ index.php
@@ -34,6 +34,8 @@
 		.page-section a:visited{color: #AC0000 !important;}
 		.nav a:hover{color: #AC0000 !important;}
 		.nav a:focus{color: #AC0000 !important;}
+		
+		a.list-group-item{color: #666 !important;}
 	
 		h1{font-size: 30pt !important;}
 		strong.num{color: #AC0000; font-size: 18pt; font-weight: bold;}
